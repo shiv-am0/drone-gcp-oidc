b36c621d611b7bc61f901750d7ebfdd23978d44e
Akshit Agrawal <me@akshitagrawal.dev>
2024-02-12 17:06:17 +0530

    credentials.json gen added

--- plugin/plugin.go
+++ plugin/plugin.go
@@ -6,8 +6,12 @@ package plugin
 
 import (
 	"context"
+	"encoding/json"
 	"fmt"
 	"os"
+	"strconv"
+	"strings"
+	"time"
 
 	"github.com/sirupsen/logrus"
 )
@@ -19,12 +23,14 @@ type Args struct {
 	// Level defines the plugin log level.
 	Level string `envconfig:"PLUGIN_LOG_LEVEL"`
 
-	OIDCToken  string `envconfig:"PLUGIN_OIDC_TOKEN_ID"`
-	ProjectID  string `envconfig:"PLUGIN_PROJECT_ID"`
-	PoolID     string `envconfig:"PLUGIN_POOL_ID"`
-	ProviderID string `envconfig:"PLUGIN_PROVIDER_ID"`
-	ServiceAcc string `envconfig:"PLUGIN_SERVICE_ACCOUNT_EMAIL_ID"`
-	Duration   string `envconfig:"PLUGIN_DURATION"`
+	OIDCToken   string `envconfig:"PLUGIN_OIDC_TOKEN_ID"`
+	ProjectID   string `envconfig:"PLUGIN_PROJECT_ID"`
+	PoolID      string `envconfig:"PLUGIN_POOL_ID"`
+	ProviderID  string `envconfig:"PLUGIN_PROVIDER_ID"`
+	ServiceAcc  string `envconfig:"PLUGIN_SERVICE_ACCOUNT_EMAIL_ID"`
+	Duration    string `envconfig:"PLUGIN_DURATION"`
+	CreateCreds bool   `envconfig:"PLUGIN_CREATE_CREDENTIALS_FILE"`
+	CredsPath   string `envconfig:"PLUGIN_CREDENTIALS_FILE_PATH"`
 }
 
 // Exec executes the plugin.
@@ -50,14 +56,26 @@ func Exec(ctx context.Context, args Args) error {
 		return err
 	}
 
-	logrus.Infof("acess token retrieved successfully\n")
-	logrus.Infof("access token set as GCLOUD_ACCESS_TOKEN\n")
+	if args.CreateCreds {
+		credsPath, err := WriteCredentialsToFile(accessToken, args.CredsPath, args.Duration, args.OIDCToken)
+		if err != nil {
+			return err
+		}
+		logrus.Infof("credentials file written to %s\n", credsPath)
 
-	if err := WriteEnvToFile(accessToken); err != nil {
-		return err
+		if err := WriteEnvToFile("GCLOUD_CREDENTIALS_FILE", accessToken); err != nil {
+			return err
+		}
+	} else {
+		logrus.Infof("acess token retrieved successfully\n")
+		logrus.Infof("access token set as GCLOUD_ACCESS_TOKEN\n")
+
+		if err := WriteEnvToFile("GCLOUD_ACCESS_TOKEN", accessToken); err != nil {
+			return err
+		}
+
+		logrus.Infof("access token written to env\n")
 	}
-
-	logrus.Infof("access token written to env\n")
 
 	return nil
 }
@@ -81,7 +99,7 @@ func VerifyEnv(args Args) error {
 	return nil
 }
 
-func WriteEnvToFile(value string) error {
+func WriteEnvToFile(key, value string) error {
 	outputFile, err := os.OpenFile(os.Getenv("DRONE_OUTPUT"), os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
 	if err != nil {
 		return fmt.Errorf("failed to open output file: %w", err)
@@ -89,10 +107,45 @@ func WriteEnvToFile(value string) error {
 
 	defer outputFile.Close()
 
-	_, err = fmt.Fprintf(outputFile, "GCLOUD_ACCESS_TOKEN=%s\n", value)
+	_, err = fmt.Fprintf(outputFile, "%s=%s\n", key, value)
 	if err != nil {
 		return fmt.Errorf("failed to write to env: %w", err)
 	}
 
 	return nil
+}
+
+func WriteCredentialsToFile(token, path, duration, idToken string) (string, error) {
+	durationInt, err := strconv.Atoi(strings.TrimSuffix(duration, "s"))
+	if err != nil {
+		return "", fmt.Errorf("failed to convert duration to int: %w", err)
+	}
+
+	path = strings.TrimSuffix(path, "/") + "/credentials.json"
+
+	expiryTime := time.Now().Add(time.Duration(durationInt) * time.Second).Format(time.RFC3339)
+
+	credentials := struct {
+		Credential struct {
+			AccessToken string  `json:"access_token"`
+			IdToken     *string `json:"id_token"`
+			TokenExpiry string  `json:"token_expiry"`
+		} `json:"credential"`
+	}{}
+
+	credentials.Credential.AccessToken = token
+	credentials.Credential.IdToken = &idToken
+	credentials.Credential.TokenExpiry = expiryTime
+
+	credentialsJSON, err := json.MarshalIndent(credentials, "", "  ")
+	if err != nil {
+		return "", fmt.Errorf("failed to marshal credentials to JSON: %w", err)
+	}
+
+	err = os.WriteFile(path, credentialsJSON, 0644)
+	if err != nil {
+		return "", fmt.Errorf("failed to write credentials file: %w", err)
+	}
+
+	return path, nil
 }
